f352b0fe5666ae51da5be5e2bc9e418cad2bcd8d
memtag14 <memtag14@gmail.com>
2026-01-15 17:09:56 +0500

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,10 @@
 import uuid
 import os
 
-def generate_filename(original_name: str) -> str:
-    ext = os.path.splitext(original_name)[1]
-    return f"{uuid.uuid4().hex}{ext}"
+def generate_filename(original_filename: str) -> str:
+    """
+    Генерирует уникальное имя файла с сохранением расширения.
+    """
+    ext = os.path.splitext(original_filename)[1]  # .png, .jpg и т.д.
+    unique_name = uuid.uuid4().hex
+    return f"{unique_name}{ext}"
